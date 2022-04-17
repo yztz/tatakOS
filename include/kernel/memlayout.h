1cfe21bac8b88751f0b6845c4d72e49b5db240fb
yztz <781089956@qq.com>
2022-04-17 16:17:32 +0800

    todi

--- include/kernel/memlayout.h
+++ include/kernel/memlayout.h
@@ -6,6 +6,7 @@
 #define MAXVA (1L << (9 + 9 + 9 + 12 - 1))
 #define IO_BASE_ADDRESS 0x1F00000000
 #define TRAMPOLINE (MAXVA - PGSIZE)
-#define KSTACK(p) (TRAMPOLINE - ((p)+1)* 2*PGSIZE)
+// #define KSTACK(p) (TRAMPOLINE - ((p)+1)* 2*PGSIZE)
+#define KSTACK (TRAMPOLINE - 2 * PGSIZE)
 #define TRAPFRAME (TRAMPOLINE - PGSIZE)
 
