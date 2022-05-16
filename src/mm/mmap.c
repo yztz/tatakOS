e0ae9afd4a965c388855d2c70d866ff4114910eb
DavidZyy <1929772352@qq.com>
2022-05-16 21:24:54 +0800

    save

---
+++ src/mm/mmap.c
@@ -0,0 +1,63 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "mm/vm.h"
+#include "param.h"
+#include "fs/stat.h"
+#include "fs/fs.h"
+#include "atomic/spinlock.h"
+#include "kernel/proc.h"
+#include "atomic/sleeplock.h"
+#include "fs/file.h"
+#include "fs/fcntl.h"
+
+// #define QUIET
+#define __MODULE_NAME__ MMAP
+#include "debug.h"
+
+#include "mm/mm_types.h"
+#include "rbtree.h"
+
+unsigned long do_mmap_pgoff(struct file * file, unsigned long addr,
+			unsigned long len, unsigned long prot,
+			unsigned long flags, unsigned long pgoff)
+{
+
+}
+
+/* Look up the first VMA which satisfies  addr < vm_end,  NULL if none. */
+struct vm_area_struct * find_vma(struct mm_struct * mm, unsigned long addr)
+{
+	struct vm_area_struct *vma = NULL;
+
+	if (mm) {
+		/* Check the cache first. */
+		/* (Cache hit rate is typically around 35%.) */
+		vma = mm->mmap_cache;
+		if (!(vma && vma->vm_end > addr && vma->vm_start <= addr)) {
+			struct rb_node * rb_node;
+
+			rb_node = mm->mm_rb.rb_node;
+			vma = NULL;
+
+			while (rb_node) {
+				struct vm_area_struct * vma_tmp;
+
+				vma_tmp = rb_entry(rb_node,
+						struct vm_area_struct, vm_rb);
+
+				if (vma_tmp->vm_end > addr) {
+					vma = vma_tmp;
+					if (vma_tmp->vm_start <= addr)
+						break;
+					rb_node = rb_node->rb_left;
+				} else
+					rb_node = rb_node->rb_right;
+			}
+			if (vma)
+				mm->mmap_cache = vma;
+		}
+	}
+	return vma;
+}
+
