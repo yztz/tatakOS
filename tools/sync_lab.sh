98a064af87dde6e13a0c4012a6855e61d14bf758
yztz <19052129@hdu.edu.cn>
2023-05-23 05:31:18 +0000

    add sync tool

---
+++ tools/sync_lab.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 默认为 push
+action="push"
+
+# 如果传入了参数，则使用传入的参数
+if [[ $# -gt 0 ]]; then
+  action="$1"
+fi
+
+if [[ "$action" == "pull" ]]; then
+  # 执行 pull 操作
+  git branch | grep "lab" | xargs -r git pull lab
+elif [[ "$action" == "push" ]]; then
+  # 执行 push 操作
+  git branch | grep "lab" | xargs -r git push lab
+else
+  # 参数错误
+  echo "错误：无效的参数 $action"
+  exit 1
+fi
